10ea013fe394e38faf852360eeee6c932756460d
Vyacheslav_Voropaev <vsvoropaev@rambler.ru>
2025-07-28 21:39:50 +0300

    test 2.0

--- conftest.py
+++ conftest.py
@@ -1,3 +1,20 @@
+import pytest
 from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 options = Options()
+# options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+browser = webdriver.Chrome(options=options)
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nоткрытие браузера для теста..")
+    options = webdriver.ChromeOptions()
+    # options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(10)
+    yield browser
+    print("\nзакрытие браузера..")
+    browser.quit()
---
+++ test_items.py
@@ -0,0 +1,6 @@
+
+link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+
+def test_find_by_key(browser):
+    browser.get(link)
+    
