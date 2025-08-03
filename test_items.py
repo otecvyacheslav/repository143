a4f70cdf9140ac2fe2f3d8bfff9c617f72a5d466
otecvyacheslav <167583351+otecvyacheslav@users.noreply.github.com>
2025-08-03 14:29:34 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_this_course(browser):
+def test_find_button_add_to_basket(browser):
     browser.get(link)
 
     find_need_element = browser.find_element(
