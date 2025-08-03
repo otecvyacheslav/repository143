3ded7a87695cdba0d48345a6c5e16336b35170ba
Vyacheslav_Voropaev <vsvoropaev@rambler.ru>
2025-08-03 13:52:30 +0300

    final version push on git

--- conftest.py
+++ conftest.py
@@ -1,12 +1,12 @@
-import pytest
-# import requests
+import pytest  # импорт пайтеста
+from selenium import webdriver  # импорт вебдрайвера
+from selenium.webdriver.chrome.options import (Options)
+# импорт работы с настройками селениума
+import time  # импорт времени
 
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import time
 
-
-def pytest_addoption(parser):
+def pytest_addoption(parser):  # парсер
+    # Парсер - это программа или скрипт, который автоматизированно извлекает и структурирует данные с веб-сайтов. Процесс извлечения данных называется парсингом. Парсеры находят и извлекают нужную информацию из HTML-кода страницы, преобразуя ее в удобный для пользователя формат
     parser.addoption(
         "--language",
         action="store",
@@ -15,21 +15,32 @@ def pytest_addoption(parser):
     )
 
 
-@pytest.fixture(scope="function")
-def browser(request):
-    user_language = request.config.getoption("--language")
-    options = Options()
-    options.add_experimental_option("prefs", {"intl.accept_language": user_language})
-    print("\nstart browser..")
-    browser = webdriver.Chrome(options=options)
-    # browser.implicitly_wait(10)
-    if user_language == "ru":
-        print("\nuse russian language..")
-    elif user_language == "es":
-        print("\nuse espanian language..")
-    else:
-        raise pytest.UsageError("--language should be 'es'")
-    yield browser
-    time.sleep(10)
-    print("\nclose browser..")
-    browser.quit()
+@pytest.fixture(
+    scope="function"
+)  # фикстура, задается правило, что отдельный запуск тест-браузера для каждой функции
+def browser(request):  # реквест (гугли, не объяснить правильно в 2-х словах)
+    user_language = request.config.getoption(
+        "--language"
+    )  # переменная = запрос настроек языка
+    options = Options()  # переменная = настройки, которые мы импортировали из селениум
+    options.add_experimental_option(
+        "prefs", {"intl.accept_language": user_language}
+    )  # Как раз те самые настройки, просто прикрепили к отдельной переменной, можно было в скобки посадить....берем работу с языком
+    print("\nstart browser..")  # вывод в консоль, что начался запуск браузера
+    options.add_argument(
+        f"--lang={user_language}"
+    )  # взяли переменную и добавили (т.к. некоторые версии Chrome и ChromeDriver лучше реагируют на `–lang` на системном уровне, который передается как аргумент командной строки.)
+    browser = webdriver.Chrome(options=options)  # запускаем хром
+    browser.implicitly_wait(10)  # добавили неявное ожидание до 10 секунд
+    if user_language == "fr":  # если используем фр
+        print("\nuse french language..")  # принт в консоль
+    elif user_language == "es":  # если используем ес
+        print("\nuse spanish language..")  # принт в консоль
+    else:  # иначе
+        raise pytest.UsageError(
+            "--language should be 'fr' or 'es'"
+        )  # выпадет ошибка с описанием в скобках
+    yield browser  # Значение выражения yield* само по себе равно последнему значению итерируемого объекта (т.е., того когда done равно true). То есть в конце передаем в browser то, что ниже
+    time.sleep(10)  # ожидание в 10 секунд
+    print("\nclose browser..")  # принт в консоль
+    browser.quit()  # закрытие браузера
--- test_items.py
+++ test_items.py
@@ -3,14 +3,15 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_this_course(browser, request):
+def test_this_course(browser):
     browser.get(link)
 
-    find_need_element = browser.find_element(By.CSS_SELECTOR, ".hidden-xs > strong")
+    find_need_element = browser.find_element(
+        By.CSS_SELECTOR, ".add-to-basket > button[value]"
+    )
     text_find_need_element = find_need_element.text
+    print(f"find {text_find_need_element}")
     assert (
-        text_find_need_element == "Total carrito:"
+        text_find_need_element == "Ajouter au panier"
+        or text_find_need_element == "Añadir al carrito"
     ), "Нет этих слов, вероятно не тот язык"
-
-
-# Total carrito:
